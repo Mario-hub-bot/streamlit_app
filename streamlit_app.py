6695cde646ae1e6635aa23b09edb921ed1c09664
Pablo Fonseca <palefo@gmail.com>
2022-06-06 17:12:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import urllib.request
+
+print('Beginning file download with urllib2...')
+
+url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'
+urllib.request.urlretrieve(url, '/Users/scott/Downloads/cat.jpg')
+
 
 n = st.slider("n", 5,100, step=1)
 chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
