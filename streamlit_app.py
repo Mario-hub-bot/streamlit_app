0250005b42dc8505418bb970f8f44b78fede5ac4
Pablo Fonseca <palefo@gmail.com>
2022-11-02 13:26:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import urllib.request
 
-print('Beginning file download with urllib2...')
-
-@st.experimental_memo
-def download_data():
-  url = 'https://files.minsa.gob.pe/s/eRqxR35ZCxrzNgr/download'
-  filename = 'data.csv'
-  urllib.request.urlretrieve(url, 'data.csv')
-  return filename
-
-
-st.write(download_data())
 
 
 n = st.slider("n", 5,100, step=1)
